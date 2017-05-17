4e34fda14f2fbf3eda546ff25f90f356386dda0d
C Isaksson <a@b.se>
2017-05-17 21:16:33 +0200

    Story 6. Testing spare fails

--- TestBowling.java
+++ TestBowling.java
@@ -9,8 +9,8 @@ public class TestBowling {
 	private Game game = new Game();
 
 	@Test
-	public void testStrike() {
-		int roll[] = { 10,0, 3,6, 7,2, 3,6, 4,4, 5,3, 3,3, 4,5, 8,1, 2,6 };
+	public void testSpare() {
+		int roll[] = { 1,9, 3,6, 7,2, 3,6, 4,4, 5,3, 3,3, 4,5, 8,1, 2,6 };
 		game.frames = roll;
 
 		assertEquals(game.calcScore(), 94);
