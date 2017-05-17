410893f320c331e537f702d38954d902e41aa9cd
C Isaksson <a@b.se>
2017-05-17 22:20:53 +0200

    Story 12. Testing with success

--- TestBowling.java
+++ TestBowling.java
@@ -9,10 +9,10 @@ public class TestBowling {
 	private Game game = new Game();
 
 	@Test
-	public void testSparesAsLastFrame() {
-		int roll[] = { 1,5, 3,6, 7,2, 3,6, 4,4, 5,3, 3,3, 4,5, 8,1, 10,0, 7,2 };
+	public void testBonusAsAStrike() {
+		int roll[] = { 1,5, 3,6, 7,2, 3,6, 4,4, 5,3, 3,3, 4,5, 8,1, 2,8, 10 };
 		game.frames = roll;
 
-		assertEquals(game.calcScore(), 92);
+		assertEquals(game.calcScore(), 93);
 	}
 }
