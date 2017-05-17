17a06a10215818715178d1bd26e1d88401df8b6d
C Isaksson <a@b.se>
2017-05-17 21:19:42 +0200

    Story 6. Added a method for recognize a spare and added it to the calcScore function

--- Game.java
+++ Game.java
@@ -9,19 +9,20 @@ public class Game {
 		for (int i = 0; i < 10; i++) {
 			if (isStrike(index)) 
 				score += 10 +frames[index+2] +frames[index+3];
-
-			
+			else if (isSpare(index))
+				score += 10 +frames[index+1];
 			else 
 				score += frames[index] +frames[index+1];
 			
-			index += 2;
-
-			
+			index += 2;		
 		}
 		return score;		
 	}
 	
 	private boolean isStrike(int index) {
 		return frames[index] == 10;
+	}
+	private boolean isSpare(int index) {
+		return frames[index] + frames[index+1] == 10;
 	}
 }
