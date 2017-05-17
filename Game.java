383891b32025076d5b11980139e6a0eaf4e21625
C Isaksson <a@b.se>
2017-05-17 20:31:17 +0200

    Implemented a function for calculating the score.

--- Game.java
+++ Game.java
@@ -2,4 +2,12 @@ package bowlingGame;
 
 public class Game {
 	protected int[] frame = new int[20];
+	
+	public int calcScore() {
+		int score = 0;
+		for (int i = 0; i < frame.length; i++)
+			score += frame[i];
+		
+		return score;		
+	}
 }
