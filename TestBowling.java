57c80840a8cfb2a423d1e0466e8da092c32f02e8
C Isaksson <a@b.se>
2017-05-17 20:35:17 +0200

    Story 5. Testing strike. Test fails.

--- TestBowling.java
+++ TestBowling.java
@@ -9,10 +9,10 @@ public class TestBowling {
 	private Game game = new Game();
 
 	@Test
-	public void testGameScore() {
-		int roll[] = { 1,5, 3,6, 7,2, 3,6, 4,4, 5,3, 3,3, 4,5, 8,1, 2,6 };
+	public void testStrike() {
+		int roll[] = { 10,0, 3,6, 7,2, 3,6, 4,4, 5,3, 3,3, 4,5, 8,1, 2,6 };
 		game.frame = roll;
 
-		assertEquals(game.calcScore(), 81);
+		assertEquals(game.calcScore(), 94);
 	}
 }
