f5ffccc118134e3ee18f10c0b0b577e48506e75b
C Isaksson <a@b.se>
2017-05-17 21:14:36 +0200

    Story 5. Retesting

--- TestBowling.java
+++ TestBowling.java
@@ -11,7 +11,7 @@ public class TestBowling {
 	@Test
 	public void testStrike() {
 		int roll[] = { 10,0, 3,6, 7,2, 3,6, 4,4, 5,3, 3,3, 4,5, 8,1, 2,6 };
-		game.frame = roll;
+		game.frames = roll;
 
 		assertEquals(game.calcScore(), 94);
 	}
