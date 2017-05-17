983cac2b3c2a08f8a030ecac838743f2e4f3eaef
C Isaksson <a@b.se>
2017-05-17 20:29:55 +0200

    Story 5. Refactored the score calculation and implemented a test case

--- TestBowling.java
+++ TestBowling.java
@@ -4,14 +4,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TestBowling {
+public class TestBowling {	
+	
+	private Game game = new Game();
 
 	@Test
-	public void testFrameScore() {
-		int[] frame = new int[] {2, 4};
-		int score = 0;
-		for (int i = 0; i < frame.length; i++ )
-			score += frame[i];
-		assertEquals(score, 6);
+	public void testGameScore() {
+		int roll[] = { 1,5, 3,6, 7,2, 3,6, 4,4, 5,3, 3,3, 4,5, 8,1, 2,6 };
+		game.frame = roll;
+
+		assertEquals(game.calcScore(), 81);
 	}
 }
